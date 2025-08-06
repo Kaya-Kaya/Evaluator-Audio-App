e43c3a7d44a1cf9c2579e74cc34e4b153cbad6c9
Ji Bing Ni <jibingni17@gmail.com>
2025-08-06 14:40:05 -0400

    removed old comments

--- src/components/ScoreDisplay.tsx
+++ src/components/ScoreDisplay.tsx
@@ -1,10 +1,8 @@
-import { NativeScrollEvent, NativeSyntheticEvent, ScrollView, StyleSheet, Text, TouchableOpacity, View, TextInput, Platform, useWindowDimensions} from "react-native";
+import { NativeScrollEvent, NativeSyntheticEvent, ScrollView, StyleSheet, Text, View, Platform, useWindowDimensions} from "react-native";
 import { useRef, useEffect, useState} from "react";
-import { Cursor, OpenSheetMusicDisplay, Fraction, SourceMeasure } from "opensheetmusicdisplay";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { Cursor, OpenSheetMusicDisplay, Fraction} from "opensheetmusicdisplay";
 import scoresData from "../musicxml/scores"; // Local mapping of score filenames to XML content
 import { WebView } from "react-native-webview";
-import AudioGenerator from "../audio/AudioGenerator";
 import React from "react";
 import { ActivityIndicator } from "react-native";
 
@@ -110,15 +108,6 @@ export default function ScoreDisplay({
       }
       cursorRef.current!.previous(); 
 
-      // if (moved < toMove && toMove < moved + delta){
-      //   console.log("======================================")
-      //   console.log("moved :", moved)
-      //   console.log("tar :", toMove)
-      //   console.log("next:", moved + delta)
-      //   console.log("======================================")
-      // }
-
-
       // Stop if we've moved enough beats
       if (moved >= toMove) {
         // console.log("moved:" ,moved, ">", toMove) 
@@ -147,18 +136,6 @@ export default function ScoreDisplay({
       }else {
         // console.log("No note under cursor.");
       }
-
-      // if (delta + moved > targetBeats) {
-      //   // Move the cursor to the next note
-      //   cursorRef.current!.previous();
-      //   cursorRef.current!.update();
-      //   osdRef.current!.render(); // Re-render the music sheet
-
-      //   console.log("delta: ", delta)
-      //   console.log("moved: ", moved)
-      //   console.log("target beat: ", targetBeats)
-      //   return
-      // }
 
       moved += delta; // Accumulate the moved beats
       movedBeats.current = moved; // Update state to reflect progress
@@ -376,8 +353,7 @@ export default function ScoreDisplay({
           if (isSmallScreen) {
             osdRef.current!.zoom = .45
           }
-          //console.log("tar:", cursorRef.current.Iterator.CurrentMeasure.ActiveTimeSignature)
-          // TODO!  Find the piece's tempo and send that instead of constant 100
+          
           dispatch({
             type: "update_piece_info",
             time_signature:
@@ -391,41 +367,6 @@ export default function ScoreDisplay({
             console.error("Error loading music XML:", error);
         });
 
-        // // Fetch the MusicXML file from the backend
-        // fetch(`http://127.0.0.1:5000/score/${state.score}`) // Replace with your actual API endpoint
-        //   .then((response) => {
-        //     if (!response.ok) {
-        //       throw new Error(`HTTP error! status: ${response.status}`);
-        //     }
-        //     return response.text(); // Return the XML content as a string
-        //   })
-        //   .then((xmlContent) => {
-        //     // Load the fetched XML content into OpenSheetMusicDisplay
-        //     return osm.load(xmlContent);
-        //   })
-        //   .then(() => {
-        //     // Render the sheet music
-        //     osm.render();
-        //     console.log("Music XML loaded successfully");
-
-        //     cursorRef.current = osm.cursor;
-        //     cursorRef.current.show(); // Ensure the cursor is visible
-        //     cursorRef.current.CursorOptions = {
-        //       ...cursorRef.current.CursorOptions,
-        //       follow: true,
-        //     };
-        //     // TODO!  Find the piece's tempo and send that instead of constant 100
-        //     dispatch({
-        //       type: "update_piece_info",
-        //       time_signature:
-        //         cursorRef.current.Iterator.CurrentMeasure.ActiveTimeSignature,
-        //       tempo: 100,
-        //     });
-        //   })
-        //   .catch((error) => {
-        //     // Handle errors in loading the music XML file
-        //     console.error("Error loading music XML:", error); // Log the error message
-        //   });
     } // Dependency array means this effect runs once when the component mounts and again when a new score is selected
   }, [dispatch, state.score, state.scores])
 
@@ -453,20 +394,6 @@ const onMessage = (event: any) => {
 
   const selectedXml = (state.scoreContents && state.scoreContents[state.score]) || scoresData[state.score] || "";
 
-  // Function used for scrolling vertically through the OSM Container based on passed in value
-  const scrollUp = (amount: number) => {
-    if (scrollViewRef.current) {
-      scrollViewRef.current.scrollTo({ y: amount, animated: false });
-    }
-  };
-
-  // Function used to listen to scroll on OSM container and saves current Y position
-  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const yOffset = event.nativeEvent.contentOffset.y;
-    // console.log('Current Scroll Position (Y):', yOffset);
-    scrollPositionRef.current = yOffset; // update ref immediately
-  };
-
   const extractTempo = (xml: string): number | null => {
     try {
       const parser = new DOMParser();
@@ -488,56 +415,6 @@ const onMessage = (event: any) => {
       return null;
     }
 };
-
-  /////////////////////////////////////////////////////////////////////////////////
-  // useEffect to tie the cursor position to the state
-  /////////////////////////////////////////////////////////////////////////////////
-//   useEffect(() => {
-//     let ct = state.cursorTimestamp; // current timestamp of cursor's note(s) in seconds
-//     var dt = new Fraction();
-//     console.log("ct:", ct);
-//     if (cursorRef.current?.Iterator.CurrentSourceTimestamp !== undefined) {
-//       var ts_meas = Fraction.createFromFraction(
-//         cursorRef.current?.Iterator.CurrentSourceTimestamp,
-//       ); // current timestamp of iterator as a fraction
-//       console.log("ts_meas:", ts_meas.RealValue);
-//       if (ct > state.timestamp) {
-//         // If timestamp is older, go back to beginning, b/c probably reset
-//         console.log("Moving ct back to beginning.");
-//         ct = 0;
-//         cursorRef.current?.reset();
-//         ts_meas = new Fraction();
-//       }
-
-//       // while timestamp is less than desired, update it
-//       while (ct <= state.timestamp) {
-//         cursorRef.current?.Iterator.moveToNextVisibleVoiceEntry(false);
-//         dt = Fraction.minus(
-//           cursorRef.current?.Iterator.CurrentSourceTimestamp,
-//           ts_meas,
-//         );
-//         // dt is a fraction indicating how much - in whole notes - the iterator moved
-
-//         ct +=
-//           (60 * dt.RealValue * state.time_signature.Denominator) /
-//           state.synth_tempo;
-//         console.log("ct:", ct);
-//         ts_meas = Fraction.plus(ts_meas, dt);
-//       }
-//       cursorRef.current?.Iterator.moveToPreviousVisibleVoiceEntry(false);
-//       console.log("Cursor should be updating");
-//       cursorRef.current?.update();
-//       dispatch({
-//         type: "cursor_update",
-//         time:
-//           (60 *
-//             cursorRef.current?.Iterator.CurrentSourceTimestamp.RealValue *
-//             state.time_signature.Denominator) /
-//           state.synth_tempo,
-//       });
-//     }
-//   },
-// [state.timestamp]);
 
   return (
     <>
@@ -567,7 +444,6 @@ const onMessage = (event: any) => {
         contentContainerStyle={{ flexGrow: 1 }}
         showsVerticalScrollIndicator
         ref={scrollViewRef}
-        onScroll={handleScroll}
         scrollEventThrottle={16}
       >
         {/* If on web, render the OSMD container like normal */}
@@ -585,11 +461,6 @@ const onMessage = (event: any) => {
           />
         )}
 
-        {/* <Text style={styles.text}>
-          <Icon name="music" size={20} color="#2C3E50" /> Reference to the SVG
-          container for sheet music{" "}
-          <Icon name="music" size={20} color="#2C3E50" />
-        </Text> */}
         {state.loadingPerformance && ( // Loading overlay
           <View style={styles.overlay}>
             <ActivityIndicator size="large" />
